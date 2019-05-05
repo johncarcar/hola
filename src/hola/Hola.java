5948257f090ff339920c3e8aa3539b23c2b711cb
jcc <jcc@Q9550>
2019-05-05 22:25:55 +0100

    Cambio 2

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -15,7 +15,7 @@ public class Hola {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola de nuevo");
         
     }
     
